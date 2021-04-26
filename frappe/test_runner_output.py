472e89c8b25af9bca13f12a67cf995cd939b482d
Faris Ansari <netchamp.faris@gmail.com>
2021-04-26 23:54:39 +0530

    fix: Show output grouped by test suite

--- frappe/test_runner_output.py
+++ frappe/test_runner_output.py
@@ -4,61 +4,74 @@
 from __future__ import unicode_literals
 import frappe
 from unittest import TextTestResult
+from unittest.util import strclass
 from simple_chalk import chalk
 from IPython.core.ultratb import ColorTB
-
 
 
 class PrettyTextTestResult(TextTestResult):
 	separator1 = "=" * 70
 	separator2 = "-" * 70
+	_test_class = None
 
 	def __init__(self, stream, descriptions, verbosity):
 		super(PrettyTextTestResult, self).__init__(stream, descriptions, 2)
 
 	def getDescription(self, test):
-		description = str(test)
-		test_name, test_module = description.split(' (')
-		test_module = test_module.rstrip(')')
+		test_name, test_module = self.getTestAndModuleName(test)
 		description = f"{chalk.white(test_name)} {chalk.gray(test_module)}"
 		return description
 
+	def getClassDescription(self, test):
+		test_class = test.__class__
+		doc = test_class.__doc__
+		if self.descriptions and doc:
+			return doc.strip().split("\n")[0].strip()
+		return strclass(test_class)
+
+	def getTestAndModuleName(self, test):
+		description = str(test)
+		test_name, test_module = description.split(" (")
+		test_module = test_module.rstrip(")")
+		return test_name, test_module
+
 	def startTest(self, test):
 		super(TextTestResult, self).startTest(test)
+		if self.showAll:
+			if self._test_class != test.__class__:
+				self._test_class = test.__class__
+				title = self.getClassDescription(test)
+				self.stream.writeln(chalk.greenBright(title))
 
 	def addSuccess(self, test):
 		if self.showAll:
-			success = chalk.bgGreen.bold.white(' PASS ')
-			description = self.getDescription(test)
-			self.stream.writeln(f"{success} {description}")
+			success = chalk.green(" ✓")
+			test_name, _ = self.getTestAndModuleName(test)
+			self.stream.writeln(f"{success} {chalk.dim(test_name)}")
 		elif self.dots:
 			self.stream.write(".")
 			self.stream.flush()
 
 	def addError(self, test, err):
-		super(TextTestResult, self).addError(test, err)
-		# self.exceptions.append((test, err))
-		if self.showAll:
-			status = chalk.bgRed.bold.white(' ERROR ')
-			description = self.getDescription(test)
-			self.stream.writeln(f"{status} {description}")
+		self.errors.append((test, err))
+		self._mirrorOutput = True
 
-			exctype, value, tb = err
-			# Skip test runner traceback levels
-			while tb and self._is_relevant_tb_level(tb):
-				tb = tb.tb_next
-			color_tb = ColorTB()
-			colored_tb = ''.join(color_tb.structured_traceback(exctype, value, tb))
-			self.stream.writeln(colored_tb)
+		if self.showAll:
+			test_name, _ = self.getTestAndModuleName(test)
+			self.stream.writeln(f"{chalk.red(' ✕')} {chalk.dim(test_name)}")
 
 		elif self.dots:
 			self.stream.write("E")
 			self.stream.flush()
 
 	def addFailure(self, test, err):
-		super(PrettyTextTestResult, self).addFailure(test, err)
+		self.failures.append((test, err))
+		self._mirrorOutput = True
+
 		if self.showAll:
-			self.stream.writeln("FAIL")
+			test_name, _ = self.getTestAndModuleName(test)
+			self.stream.writeln(f"{chalk.red(' ✕')} {chalk.dim(test_name)}")
+
 		elif self.dots:
 			self.stream.write("F")
 			self.stream.flush()
@@ -95,7 +108,18 @@ class PrettyTextTestResult(TextTestResult):
 
 	def printErrorList(self, flavour, errors):
 		for test, err in errors:
-			self.stream.writeln(self.separator1)
-			self.stream.writeln("%s: %s" % (flavour, self.getDescription(test)))
-			self.stream.writeln(self.separator2)
-			self.stream.writeln("%s" % err)
+			self.printError(test, err, flavour)
+
+	def printError(self, test, err, type):
+		self.stream.writeln()
+		status = chalk.bgRed.bold.white(f" {type} ")
+		test_name, _ = self.getTestAndModuleName(test)
+		self.stream.writeln(f"{status} {chalk.red.bold(test_name)}")
+		self.stream.writeln()
+		exctype, value, tb = err
+		# Skip test runner traceback levels
+		while tb and self._is_relevant_tb_level(tb):
+			tb = tb.tb_next
+		color_tb = ColorTB()
+		colored_tb = "".join(color_tb.structured_traceback(exctype, value, tb))
+		self.stream.writeln(colored_tb)
