6809ddebeae77ab2022ca4f10c3165f7383d6218
Faris Ansari <netchamp.faris@gmail.com>
2026-07-06 00:56:16 +0530

    fix(docs): scan packaged frappe-ui styles

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -6,6 +6,8 @@ export default {
 	content: [
 		"./.vitepress/**/*.{js,ts,vue}",
 		"./content/**/*.md",
+		"./node_modules/frappe-ui/vitepress/**/*.{js,ts,vue}",
+		"./node_modules/frappe-ui/src/**/*.{js,ts,vue}",
 		"../../frappe-ui/vitepress/**/*.{js,ts,vue}",
 		"../../frappe-ui/src/**/*.{js,ts,vue}",
 	],
